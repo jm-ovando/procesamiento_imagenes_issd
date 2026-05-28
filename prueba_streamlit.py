464659d86c895aab9d766136409685e314db0f6d
Juan Manuel Ovando <jm.ovando22@gmail.com>
2026-05-28 01:45:16 +0000

    Prueba de archivo .py con streamlit para subirse a streamlit cloud

---
+++ prueba_streamlit.py
@@ -0,0 +1,7 @@
+#aaaaaaaaaaaaa
+
+import streamlit as st
+st.title("Mi primer dashboard en la nube")
+st.write("Estoy probando si esta mierda funciona o no")
+st.header("si, si funciona jeje")git add
+
