3173b8cbf537363681413c7dec3b98a5a274e50f
Juan Manuel Ovando <jm.ovando22@gmail.com>
2026-05-28 01:49:33 +0000

    actualización 1 - se corrigen errores de sintaxis

--- prueba_streamlit.py
+++ prueba_streamlit.py
@@ -1,7 +1,7 @@
-#aaaaaaaaaaaaa
+#Prueba de archivo .py subido a Streamlit. Se linkea con Streamlit Cloud 
+#para hacer las pruebas a traves de github
 
 import streamlit as st
 st.title("Mi primer dashboard en la nube")
 st.write("Estoy probando si esta mierda funciona o no")
-st.header("si, si funciona jeje")git add
-
+st.header("si, si funciona jeje")
